4da2b8aa0b5e06c375f7439fdb78d265e5315b7f
lihsuan711 <83916251+lihsuan711@users.noreply.github.com>
2021-05-22 21:58:26 +0800

    Update message.py

--- message.py
+++ message.py
@@ -7,8 +7,8 @@ auth_token="dd73d77310cd5afb1f73fa2bb7c2bb5d"
 
 client = Client(account_sid, auth_token)
 message = client.messages.create( # sending msg; client.messages.create is a fcn call 
-    to="+886983558907",
+    to="+886983000000",
     from_="+15598887033",
-    body="hi!我是俐璇~收到簡訊後,趕快打電話來祝賀我成功了")
+    body="hi!收到簡訊後,趕快打電話來祝賀我成功了")
 
 print(message.sid)
